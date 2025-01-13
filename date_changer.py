260686801e633c5da905b698406c5b4ca407a710
LanaAntonova <33715009+LanaAntonova@users.noreply.github.com>
2025-01-13 09:53:21 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,13 +1,5 @@
-username = "Евгений"
-title = "Очень важная заметка"
-content = "Написать заявление на отпуск"
-status = "В работе"
 created_date = "01-01-2025"
 issue_date = "01-04-2025"
 
-print("Имя пользователя: ", username)
-print("Заголовок заметки: ", title)
-print("Описание заметки: ", content)
-print("Статус заметки: ", status)
-print("Дата создания заметки в формате DD-MM-YYYY: ", created_date[0:5])
-print("Дата истечения заметки (дедлайн) в формате DD-MM-YYYY: ", issue_date[0:5])
+print("Дата создания заметки в формате DD-MM: ", created_date[0:5])
+print("Дата истечения заметки (дедлайн) в формате DD-MM: ", issue_date[0:5])
