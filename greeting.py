5378d9bd8ff8c638af057ce70d7d69ac235f5525
LanaAntonova <33715009+LanaAntonova@users.noreply.github.com>
2025-01-13 09:36:57 +0300

    add variables
    Этап 1: Задание 1.
    This file contains variables

---
+++ greeting.py
@@ -0,0 +1,12 @@
+username = "Евгений"
+print("Имя пользователя: ", username)
+title = "Очень важная заметка"
+print("Заголовок заметки: ", title)
+content = "Написать заявление на отпуск"
+print("Описание заметки: ", content)
+status = "В работе"
+print("Статус заметки: ", status)
+created_date = "01-01-2025"
+print("Дата создания заметки в формате DD-MM-YYYY: ", created_date)
+issue_date = "01-04-2025"
+print("Дата истечения заметки (дедлайн) в формате DD-MM-YYYY: ", issue_date)
